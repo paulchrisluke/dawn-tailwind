09dcceb09cf7eecf2c4380771880ee511df8f26d
Danny Venn <freedannyv@gmail.com>
2022-09-30 16:39:08 +0700

    fixes social lookbook responsive and adds the is_active query

--- assets/social-lookbook-feed.js
+++ assets/social-lookbook-feed.js
@@ -1,19 +1,37 @@
-fetch("https://app.iloveugc.com/api/v1/feed/3?page=3&per_page=15")
-  .then((response) => response.json())
-  .then((results) => {
-    const feed = results.data;
-    if (feed.length) {
-      let swiper_wrapper = document.querySelector(".swiper-wrapper");
-      if (swiper_wrapper)
-        feed
-          .sort((a, b) => b.id - a.id)
-          .filter((item) => item.is_active)
-          .forEach((item) => {
-            swiper_wrapper.appendChild(createImageCard(item));
-          });
-    }
-  })
-  .catch((e) => console.log(e));
+let pagination_url = "";
+
+const initialFetch = async () => {
+  // add &per_page=10 for pagination
+  let response = await fetch(
+    "https://app.iloveugc.com/api/v1/feed/3?page=3&filter[is_active]=true"
+  );
+  let feedItems = await response.json();
+  await addNewItemsToFeed(feedItems.data);
+  // pagination_url = feedItems.next_page_url;
+};
+
+const paginateNextPage = async () => {
+  let response = await fetch(pagination_url);
+  let newFeedItems = await response.json();
+  await addNewItemsToFeed(newFeedItems.data);
+};
+
+function addNewItemsToFeed(feed) {
+  if (feed.length) {
+    let swiper_wrapper = document.querySelector(".swiper-wrapper");
+    feed
+      .sort(function (a, b) {
+        return a.created_at > b.created_at
+          ? -1
+          : a.created_at < b.created_at
+          ? 1
+          : 0;
+      })
+      .forEach((item) => {
+        swiper_wrapper.appendChild(createImageCard(item));
+      });
+  }
+}
 
 function createImageCard(item) {
   let new_gallery_item;
@@ -34,9 +52,58 @@ function createImageCard(item) {
     "border:2px solid black;box-shadow:black 3px 3px 0 0;";
 
   if (item.type === "image") {
-    new_gallery_item.innerHTML = `<img src="${item.thumbnail_url}" class="xw-full xh-full xobject-cover xobject-center">`;
+    new_gallery_item.innerHTML = `<img data-src="${item.thumbnail_url}" class="xw-full xh-full xobject-cover xobject-center">`;
   } else {
-    new_gallery_item.innerHTML = `<video src="${item.content_url}" class="xw-full xh-full xobject-cover xobject-center" autoplay muted loop playsinline></video>`;
+    new_gallery_item.innerHTML = `<video data-src="${item.content_url}" class="xw-full xh-full xobject-cover xobject-center" autoplay muted loop playsinline></video>`;
   }
   return new_gallery_item;
 }
+
+window.addEventListener("load", async () => {
+  await initialFetch();
+  if (!!window.IntersectionObserver) {
+    // let slides = document.querySelectorAll(".swiper-slide");
+    let observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          let media = entry.target.firstChild;
+
+          // update media src with data-src
+          media.src = media.dataset.src;
+
+          // if video, play or pause
+          if (media.tagName === "VIDEO") {
+            let isPaused = false;
+            if (entry.intersectionRatio != 1 && !media.paused) {
+              media.pause();
+              isPaused = true;
+            } else if (isPaused) {
+              media.play();
+              isPaused = false;
+            }
+          }
+
+          console.log(
+            "%csocial-lookbook-feed.js line:86 media",
+            "color: #007acc;",
+            entry
+          );
+
+          // pagination check
+          // if (entry.target === slides[slides.length - 1]) {
+          //   observer.disconnect();
+          //   paginateNextPage();
+          //   document.querySelectorAll(".swiper-slide").forEach((item) => {
+          //     observer.observe(item);
+          //   });
+          // }
+
+          observer.unobserve(entry.target);
+        }
+      });
+    }, {});
+    document.querySelectorAll(".swiper-slide").forEach((item) => {
+      observer.observe(item);
+    });
+  }
+});
